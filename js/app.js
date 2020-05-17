d2dda15842c4851aee46335dfeca17b1e60ba6ad
Amir Sohel <amirsohel@Amirs-MBP.lan>
2020-05-17 14:07:27 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,131 @@
+const restaurants =  [
+  {
+    id: 1,
+    name: "Mission Chinese Food",
+    price: "₹ 534",
+    photograph: "https://b.zmtcdn.com/data/collections/2e5c5dc79d4b465797c8e425d814f42c_1582256432.jpg",
+    address: "454 Van Brunt St, Brooklyn, NY 11231",
+    location: "Vasant Kunj",
+    raiting:4,
+    type: "Veg"
+  }, {
+    "id": 2,
+    name: "Emily",
+    price: "₹ 128",
+    photograph: "https://b.zmtcdn.com/data/collections/532af3ab24d29dbddcdcd16921ada06f_1535628640.jpg",
+    address: "454 Van Brunt St, Brooklyn, NY 11231",
+    location: "MG Road, Gurgaon",
+    raiting:3,
+    type: "Non-veg"
+  }, {
+    "id": 3,
+    name: "Kang Ho Dong Baekjeong",
+    price: "₹ 300",
+    photograph: "https://b.zmtcdn.com/data/collections/105153a6d17514c0b1e32900a56c58e5_1584436497.jpg",
+    address: "454 Van Brunt St, Brooklyn, NY 11231",
+    location: "Sushant Lok",
+    raiting:5,
+    type: "Indian"
+  }, {
+    "id": 4,
+    name: "Katz's Delicatessen",
+    price: "₹ 159",
+    photograph: "https://b.zmtcdn.com/data/collections/4c645d68d0ad4c624abab81374c8997c_1582106595.jpg",
+    address: "205 E Houston St, New York, NY 10002",
+    location: "Nehru Place",
+    raiting:3,
+    type: "Italian"
+  }, {
+    "id": 5,
+    name: "Roberta's Pizza",
+    price: "₹ 99",
+    photograph: "https://b.zmtcdn.com/data/collections/d5fee6a5e24cda1629f810c3836dcb1f_1582106775.jpg",
+    address: "261 Moore St, Brooklyn, NY 11206",
+    location:"Chanakyapuri",
+    raiting:4,
+    type: "Non-veg"
+  }, {
+    "id": 6,
+    name: "Hometown BBQ",
+    price: "₹ 450",
+    photograph: "https://b.zmtcdn.com/data/collections/bd128475c037832b46960ef9b2910b05_1582201433.jpg",
+    address: "454 Van Brunt St, Brooklyn, NY 11231",
+    location:"Barakhamba Road",
+    raiting:3,
+    type: "Indian"
+  }, {
+    "id": 7,
+    name: "Superiority Burger",
+    price: "₹ 149",
+    photograph: "https://b.zmtcdn.com/data/collections/25b0494ade2359d4e6d90e1c73ea6672_1582082248.jpg",
+    address: "430 E 9th St, New York, NY 10009",
+    location:"Mehrauli",
+    raiting:5,
+    type: "Veg"
+  }, {
+    "id": 8,
+    name: "The Dutch",
+    price: "₹ 139",
+    photograph: "https://b.zmtcdn.com/data/collections/f672e9556e44fe33ff47bf751639c0bd_1535356450.jpg",
+    address: "131 Sullivan St, New York, NY 10012",
+    location:"Delhi",
+    raiting:5,
+    type: "Veg"
+  }, {
+    "id": 9,
+    name: "Mu Ramen",
+    price: "₹ 249",
+    photograph: "https://b.zmtcdn.com/data/collections/25b0494ade2359d4e6d90e1c73ea6672_1582082248.jpg",
+    address: "1209 Jackson Ave, Queens, NY 11101",
+    location:"Sector 29, Gurgaon",
+    raiting:4,
+    type: "Italian"
+  }, {
+    "id": 10,
+    name: "Casa Enrique",
+    price: "₹ 299",
+    photograph: "https://b.zmtcdn.com/data/collections/e1d5e8f900dbca01d11f353ef4b6a97c_1581661395.jpg",
+    address: "5-48 49th Ave, Queens, NY 11101",
+    location:"Aerocity",
+    raiting:3,
+    type: "Non-veg"
+  }
+]
+
+function renderRestaurentCardsView(restaurants) {
+  restaurants.forEach((shop) => {
+    document.querySelector(".restaurent-listing").insertAdjacentHTML(
+      "afterbegin",
+      `<div class="col-sm-4">
+      <div class="restaurent-card mb-4 position-relative">
+      <span class="badge badge-success">${shop.type}</span>
+          <div class="img-wrapper position-relative">
+          <h3 class="restaurent-title">${shop.name}</h3>
+          <ul class="raiting p-0 m-0 list-unstyled d-flex">
+           ${raitingStar(`${shop.raiting}`)} 
+         </ul>
+              <img class="w-100" src=${shop.photograph} alt="">
+          </div>
+          <div class="restaurent-info p-3">
+              <p class="mb-0 location">${shop.location}</p>
+              <p class="mb-1">${shop.address}</p>
+              <p class="mb-0 price">${shop.price}</p>
+          </div>
+      </div>
+  </div>`
+    );
+  });
+}
+
+
+//Genrate start rating based on number argument
+function raitingStar(raitingNumber) {
+  let raiting_list = "";
+  for (let i = 0; i < raitingNumber; i++) {
+    raiting_list += `<li><i class="fa fa-star" aria-hidden="true"></i></li>`;
+  }
+  return raiting_list;
+}
+
+
+renderRestaurentCardsView(restaurants)
